a1d2a1a5045a1128a7de3b52e23af5f5b7654bef
Yash Aryan <yasharyan307@outlook.com>
2022-02-24 00:44:21 +0530

    Template edit

---
+++ app/routes/login/index.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Login to your account</h1>
+    </div>
+  );
+}
