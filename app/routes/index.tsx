fc05e492c131d72204dbf5debbb353bdd4c41235
Yash Aryan <yasharyan307@outlook.com>
2022-03-24 00:17:07 +0530

    Add: Login and Home pages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,42 @@
-export default function Index() {
+import { Link } from "remix";
+
+export default function Home() {
   return (
-    <div>
-      <h1 className="text-red-400">ADR Portal | KMC</h1>
+    <div className="w-screen h-screen flex flex-col pt-14">
+      <div className="rounded-lg w-3/4 min-h-3/4 mx-auto p-10 text-left">
+        <h2 className="text-gray-500 text-2xl">Select a form to fill</h2>
+
+        <div className="grid grid-cols-3 grid-rows-1 gap-5">
+          <CardSelect
+            route="adr-reporting/1"
+            image="1"
+            name="ADR Reporting Form"
+          />
+          <CardSelect
+            route="suspected-adr-reporting/1"
+            image="2"
+            name="Suspected ADR Reporting Form"
+          />
+          <CardSelect
+            route="medical-device-reporting/1"
+            image="3"
+            name="Medical Device ADR Reporting Form"
+          />
+        </div>
+      </div>
     </div>
   );
 }
+
+const CardSelect = (props: { name: string; route: string; image?: string }) => (
+  <Link to={props.route} className="">
+    <div className="border-2 rounded-lg border-gray-400 p-3 h-full hover:scale-105 duration-75">
+      <div className="w-full h-full flex flex-col justify-start">
+        <img src="https://via.placeholder.com/150" className="h-3/4 w-full" />
+        <p className="font-medium text-xl text-black pt-5 text-center">
+          {props.name}
+        </p>
+      </div>
+    </div>
+  </Link>
+);
--- app/routes/login/index.tsx
+++ app/routes/login/index.tsx
@@ -1,7 +1,40 @@
+import { Input, Checkbox } from "antd";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+
 export default function Login() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Login to your account</h1>
+    <div className="w-screen h-screen flex flex-col justify-center">
+      <div className="shadow-xl rounded-lg w-1/2 min-h-2/4 mx-auto mb-32 p-10 text-center">
+        <h1 className="text-[#6C567B] text-[35px]">ADR Portal</h1>
+        <form className="w-1/2 mx-auto">
+          <div className="text-left mb-5">
+            <label>Email</label>
+            <Input placeholder="example@email.com" />
+          </div>
+          <div className="text-left mb-5">
+            <label>Password</label>
+            <Input.Password
+              placeholder="YourPasswordHere"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            />
+          </div>
+          <div className="text-left mb-5 flex flex-row justify-center">
+            <Checkbox className="font-medium">Remember me</Checkbox>
+          </div>
+          <div className="mb-10">
+            <button className="w-1/2 bg-[#6C567B] text-white rounded-lg p-2">
+              Login
+            </button>
+          </div>
+          <div>
+            <p className="underline underline-offset-4 text-gray-500">
+              Forgot password?
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
